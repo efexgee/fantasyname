abe73db55b008515610a7f82cf50be2d1e1a5aaf
Falko Goettsch <falko@uw.edu>
2018-12-02 20:55:54 -0800

    added some examples

---
+++ c/examples.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+
+./namegen '!Bv<;Bvcv|kt;v>'
+./namegen '!(qu<v>l|th<vC>);(th|s|qu)vc'
+./namegen '!<s|Bvs> !ss'
+./namegen '!<s|BVs> !ss(heim|berg|burg|stein|son)'
+
+set +x
